824d4807ac154bb421f58069bf98b4ce007fe663
ridhaadev <ridhaadev@gmail.com>
2022-04-24 19:53:59 +0200

    Finished up pricing cards, added larger description modal onclick

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,30 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
-export default function Pizza( { pizza } ) {
+export default function Pizza({ pizza }) {
+  const [quantity, setQuantity] = useState(1);
+  const [varient, setVarient] = useState("small");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>
+    <div
+      style={{ margin: "70px" }}
+      className="shadow-lg p-3 mb-5 bg-white rounded"
+    >
+      <div onClick={handleShow}>
         <h1>{pizza.name}</h1>
-        <img src={pizza.image} className="img-fluid pizza-img" />
+        <img src={pizza.image} className="pizza-preview-img" />
+      </div>
 
-        <div className="flex-container">
-            <div className='w-100'>
-                <p>Variants</p>
-                <select>
-                {pizza.varients.map(varient => {
-                    return <option value={varient}>{varient}</option>
-                })}
-                </select>
-                
-            </div>
-            <div className='w-100'>
-                <p>Quantity</p>
-                <select>
-                    { [...Array(10).keys()].map((x, i) => {
-                        return <option value={i + 1}>{i + 1}</option>
-                    }) }
-                </select>
-            </div>
+      <div className="flex-container">
+        <div className="w-100 m-1">
+          <p>Variants</p>
+          <select
+            className="form-control m-1"
+            value={varient}
+            onChange={(e) => {
+              setVarient(e.target.value);
+            }}
+          >
+            {pizza.varients.map((varient) => {
+              return <option value={varient}>{varient}</option>;
+            })}
+          </select>
         </div>
+        <div className="w-100 m-1">
+          <p>Quantity</p>
+          <select
+            className="form-control"
+            value={quantity}
+            onChange={(e) => {
+              setQuantity(e.target.value);
+            }}
+          >
+            {[...Array(10).keys()].map((x, i) => {
+              return <option value={i + 1}>{i + 1}</option>;
+            })}
+          </select>
+        </div>
+      </div>
+      <div className="flex-container">
+        <div className="m-1 w-100">
+          <h1 className="nt-1">
+            Price: R{pizza.prices[0][varient] * quantity}
+          </h1>
+        </div>
+        <div className="m-1 w-100">
+          <button className="btn">ADD TO CART</button>
+        </div>
+      </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <img src={pizza.image} alt="" className="img-fluid pizza-modal-img" />
+		  <p>{pizza.description}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className="btn" onClick={handleClose}>
+            CLOSE
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
-  )
+  );
 }
